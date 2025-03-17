160c41629c99544f56253befc73cf43734cfea3a
Ewan-Wills <ewills172@gmail.com>
2025-03-17 15:43:14 +0000

    Servos

--- src/demo1.cpp
+++ src/demo1.cpp
@@ -1,41 +1,41 @@
-#define STEP_X 2
-#define DIR_X 3
-#define STEP_Y 4
-#define DIR_Y 5
+// #define STEP_X 2
+// #define DIR_X 3
+// #define STEP_Y 4
+// #define DIR_Y 5
 
-#define STEPS_PER_MM 100  // Adjust based on motor & mechanism
+// #define STEPS_PER_MM 100  // Adjust based on motor & mechanism
 
-void moveStepper(int stepPin, int dirPin, int steps, bool direction) {
-    digitalWrite(dirPin, direction);  // Set direction
-    for (int i = 0; i < abs(steps); i++) {
-        digitalWrite(stepPin, HIGH);
-        delayMicroseconds(500);  // Adjust for speed
-        digitalWrite(stepPin, LOW);
-        delayMicroseconds(500);
-    }
-}
+// void moveStepper(int stepPin, int dirPin, int steps, bool direction) {
+//     digitalWrite(dirPin, direction);  // Set direction
+//     for (int i = 0; i < abs(steps); i++) {
+//         digitalWrite(stepPin, HIGH);
+//         delayMicroseconds(500);  // Adjust for speed
+//         digitalWrite(stepPin, LOW);
+//         delayMicroseconds(500);
+//     }
+// }
 
-void moveToXY(int x_target, int y_target, int x_current, int y_current) {
-    int stepsX = (x_target - x_current) * STEPS_PER_MM;
-    int stepsY = (y_target - y_current) * STEPS_PER_MM;
+// void moveToXY(int x_target, int y_target, int x_current, int y_current) {
+//     int stepsX = (x_target - x_current) * STEPS_PER_MM;
+//     int stepsY = (y_target - y_current) * STEPS_PER_MM;
 
-    bool dirX = stepsX > 0;
-    bool dirY = stepsY > 0;
+//     bool dirX = stepsX > 0;
+//     bool dirY = stepsY > 0;
 
-    moveStepper(STEP_X, DIR_X, stepsX, dirX);
-    moveStepper(STEP_Y, DIR_Y, stepsY, dirY);
-}
+//     moveStepper(STEP_X, DIR_X, stepsX, dirX);
+//     moveStepper(STEP_Y, DIR_Y, stepsY, dirY);
+// }
 
-void setup() {
-    pinMode(STEP_X, OUTPUT);
-    pinMode(DIR_X, OUTPUT);
-    pinMode(STEP_Y, OUTPUT);
-    pinMode(DIR_Y, OUTPUT);
-}
+// void setup() {
+//     pinMode(STEP_X, OUTPUT);
+//     pinMode(DIR_X, OUTPUT);
+//     pinMode(STEP_Y, OUTPUT);
+//     pinMode(DIR_Y, OUTPUT);
+// }
 
-void loop() {
-    moveToXY(10, 10, 0, 0);  // Move from (0,0) to (10,10)
-    delay(2000);
-    moveToXY(0, 0, 10, 10);  // Move back
-    delay(2000);
-}
+// void loop() {
+//     moveToXY(10, 10, 0, 0);  // Move from (0,0) to (10,10)
+//     delay(2000);
+//     moveToXY(0, 0, 10, 10);  // Move back
+//     delay(2000);
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
-
 #include <Stepper.h>
-
+#include <ESP32Servo.h>
 // change this to the number of steps on your motor
 #define STEPS 200
 
@@ -9,42 +8,12 @@
 #define xmotor1Pin1 12
 #define xmotor1Pin2 14
 
-// #define xmotor2Pin1 32
-// #define xmotor2Pin2 33
+#define servo1Pin 18
+#define servo2Pin 19
+Servo servo1;
+Servo servo2;
 
-// #define ymotorPin1 25
-// #define ymotorPin2 26
 
-// #include <AccelStepper.h>
-
-// // Define some steppers and the pins the will use
-// //AccelStepper stepper1; // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
-
-// AccelStepper stepper1(AccelStepper::FULL2WIRE, xmotor1Pin1, xmotor1Pin2);
-
-// void setup()
-// {  
-//   Serial.begin(115200);
-//     stepper1.setMaxSpeed(1000.0);
-//     stepper1.setAcceleration(1000.0);
-//     stepper1.moveTo(240);
-    
-   
-// }
-
-// void loop()
-// {
-//   Serial.println("hello");
-//     // Change direction at the limits
-//     if (stepper1.distanceToGo() == 0){
-// 	    stepper1.moveTo(-stepper1.currentPosition());
-//     }
-//     stepper1.run();
-
-// }
-
-#define STEP_PIN 12
-#define DIR_PIN 14
 
 int stepDelay = 2000;  // Initial delay between steps (microseconds)
 int minStepDelay = 500;  // Minimum delay (maximum speed)
@@ -53,31 +22,49 @@ int totalSteps = 200;  // Total steps (1 full rotation for 1.8° step motor)
 int stepCount = 0;  // Track the number of steps taken
 
 void setup() {
-    pinMode(STEP_PIN, OUTPUT);
-    pinMode(DIR_PIN, OUTPUT);
-    digitalWrite(DIR_PIN, HIGH);  // Set motor direction
+    Serial.begin(115200);
+    servo1.attach(servo1Pin);
+    servo2.attach(servo2Pin);
+
+    pinMode(xmotor1Pin1, OUTPUT);
+    pinMode(xmotor1Pin2, OUTPUT);
+    digitalWrite(xmotor1Pin2, HIGH);  // Set motor direction
 }
 
 void loop() {
     // Gradually accelerate
-    while (stepCount < totalSteps) {
-        digitalWrite(STEP_PIN, HIGH);
-        delayMicroseconds(stepDelay);  // Wait for the step delay
-        digitalWrite(STEP_PIN, LOW);
-        delayMicroseconds(stepDelay);  // Wait for the next step
+    // while (stepCount < totalSteps) {
+    //     digitalWrite(STEP_PIN, HIGH);
+    //     delayMicroseconds(stepDelay);  // Wait for the step delay
+    //     digitalWrite(STEP_PIN, LOW);
+    //     delayMicroseconds(stepDelay);  // Wait for the next step
         
-        // Increase speed by reducing the delay
-        if (stepDelay > minStepDelay) {
-            stepDelay -= acceleration;  // Increase speed (decrease delay)
-        }
+    //     // Increase speed by reducing the delay
+    //     if (stepDelay > minStepDelay) {
+    //         stepDelay -= acceleration;  // Increase speed (decrease delay)
+    //     }
         
-        stepCount++;  // Increment step counter
+    //     stepCount++;  // Increment step counter
+    // }
+    
+    // delay(1000);  // Wait after completing a full rotation
+    
+    // // Reverse direction for the next loop
+    // digitalWrite(DIR_PIN, !digitalRead(DIR_PIN));  // Reverse direction
+    // stepCount = 0;  // Reset step count
+    // stepDelay = 2000;  // Reset the step delay to initial value
+
+    for(int posDegrees = 0; posDegrees <= 180; posDegrees++) {
+        servo1.write(posDegrees);
+        servo2.write(posDegrees);
+        Serial.println(posDegrees);
+        delay(20);
+      }
+    
+      for(int posDegrees = 180; posDegrees >= 0; posDegrees--) {
+        servo1.write(posDegrees);
+        servo2.write(posDegrees);
+        Serial.println(posDegrees);
+        delay(20);
+      }
     }
-    
-    delay(1000);  // Wait after completing a full rotation
-    
-    // Reverse direction for the next loop
-    digitalWrite(DIR_PIN, !digitalRead(DIR_PIN));  // Reverse direction
-    stepCount = 0;  // Reset step count
-    stepDelay = 2000;  // Reset the step delay to initial value
-}
