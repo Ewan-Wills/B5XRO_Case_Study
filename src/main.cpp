e99b2c964149e1dbc300e6dcc1f70e612886a08c
Ewan-Wills <ewills172@gmail.com>
2025-03-26 13:17:25 +0000

    working picking up and placing bolts

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,20 @@
 #define xmotor1Pin1 12
 #define xmotor1Pin2 14
 
-#define servo1Pin 18
-#define servo2Pin 19
-Servo servo1;
-Servo servo2;
+#define rackServoPin 19
+#define screwServoPin 18
+Servo rackServo;
+Servo screwServo;
 
+//values for end effector
+#define rackBottom 180
+#define rackTop 0
 
+#define screwOff 90
+#define screwIn 180
+#define screwOut 0
+//speed at which the rack will go up/down. Lower means faster
+#define rackSpeed 100
 
 int stepDelay = 2000;  // Initial delay between steps (microseconds)
 int minStepDelay = 500;  // Minimum delay (maximum speed)
@@ -21,50 +29,72 @@ int acceleration = 10;  // Acceleration factor (lower value = faster acceleratio
 int totalSteps = 200;  // Total steps (1 full rotation for 1.8° step motor)
 int stepCount = 0;  // Track the number of steps taken
 
+void screw(int value){
+  screwServo.write(value);
+}
+void rackDown (int speed){
+  if (speed==-1){
+    rackServo.write(rackBottom);
+  }else{
+    for (int i = rackTop; i<=rackBottom; i++){
+      rackServo.write(i);
+      delay(speed);
+    }
+  }
+}
+void rackUp(int speed){
+  if (speed==-1){
+    rackServo.write(rackTop);
+  }else{
+    for (int i = rackBottom; i>=rackTop; i--){
+      rackServo.write(i);
+      delay(speed);
+  }
+  }
+}
+void pickUp(){
+  rackDown(-1);
+  delay(100);
+  screw(screwOut);
+  delay(500);
+  rackUp(rackSpeed);
+  screw(screwOff);
+}
+
+void putDown(){
+  rackUp(-1);
+  delay(100);
+  screw(screwIn);
+  rackDown(rackSpeed);
+  delay(100);
+  screw(screwOff);
+  rackUp(-1);
+}
 void setup() {
     Serial.begin(115200);
-    servo1.attach(servo1Pin);
-    servo2.attach(servo2Pin);
+    rackServo.attach(rackServoPin);
+    screwServo.attach(screwServoPin);
 
     pinMode(xmotor1Pin1, OUTPUT);
     pinMode(xmotor1Pin2, OUTPUT);
     digitalWrite(xmotor1Pin2, HIGH);  // Set motor direction
+    rackServo.write(rackTop);
+    screw(screwOff);
+    delay(100);
 }
-
 void loop() {
-    // Gradually accelerate
-    // while (stepCount < totalSteps) {
-    //     digitalWrite(STEP_PIN, HIGH);
-    //     delayMicroseconds(stepDelay);  // Wait for the step delay
-    //     digitalWrite(STEP_PIN, LOW);
-    //     delayMicroseconds(stepDelay);  // Wait for the next step
-        
-    //     // Increase speed by reducing the delay
-    //     if (stepDelay > minStepDelay) {
-    //         stepDelay -= acceleration;  // Increase speed (decrease delay)
-    //     }
-        
-    //     stepCount++;  // Increment step counter
-    // }
     
-    // delay(1000);  // Wait after completing a full rotation
     
-    // // Reverse direction for the next loop
-    // digitalWrite(DIR_PIN, !digitalRead(DIR_PIN));  // Reverse direction
-    // stepCount = 0;  // Reset step count
-    // stepDelay = 2000;  // Reset the step delay to initial value
+    // Serial.println("Picking Up");
+    // pickUp();
+    
+    // Serial.println("Waiting");
+    // delay(5000);
 
-    for(int posDegrees = 0; posDegrees <= 180; posDegrees++) {
-        servo1.write(posDegrees);
-        servo2.write(posDegrees);
-        Serial.println(posDegrees);
-        delay(20);
-      }
+    // Serial.println("Placing bolt");
+    // putDown();
     
-      for(int posDegrees = 180; posDegrees >= 0; posDegrees--) {
-        servo1.write(posDegrees);
-        servo2.write(posDegrees);
-        Serial.println(posDegrees);
-        delay(20);
-      }
+    // Serial.println("Waiting");
+    // delay(5000);
+
     }
